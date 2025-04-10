b32f23af618d2267027d0b0d563a3a0f34352ae8
Zek <nzekic@hotmail.com>
2025-04-10 18:32:31 +0200

    Test

--- message_app/core/urls.py
+++ message_app/core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('msgs/', views.msg_list, name='msg_list'),
+    path('msgs/test', views.msg_list, name='test'),
     path('api/msgs/', views.MsgList.as_view(), name='api_msg_list')
 ]
